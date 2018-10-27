13ae271362a9b3a3550f0abe6d62d61c2bd5cd4c
Jinal Shah <>
2018-10-27 03:29:35 -0700

    no message

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Advanced-Objects-Introduction.js
@@ -0,0 +1,39 @@
+const robot = {
+  model: 'B-4MI',
+  mobile: true,
+  greetMaster() {
+  	console.log(`I'm model ${this.model}, how may I be of service?`);
+  }
+}
+
+const massProdRobot = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    greetMaster() {
+      console.log(`I'm model ${this.model}, how may I be of service?`);
+    }
+  }
+}
+
+const shinyNewRobot = massProdRobot('TrayHax', true)
+
+const chargingStation = {
+  _name: 'Electrons-R-Us',
+  _robotCapacity: 120,
+  _active: true,
+  _chargingRooms: ['Low N Slow', 'Middle of the Road', 'In and Output'],
+
+  set robotCapacity(newCapacity) {
+    if (typeof newCapacity === 'number') {
+      this._robotCapacity = newCapacity;
+    } else {
+      console.log(`Change ${newCapacity} to a number.`)
+    }
+  },
+  get robotCapacity() {
+    return this._robotCapacity;
+  }
+}
+
+/*This lesson will cover how to use the this keyword. Conveying privacy in JavaScript, defining getters and setters in objects, creating factory functions, and using destructuring techniques.*/
