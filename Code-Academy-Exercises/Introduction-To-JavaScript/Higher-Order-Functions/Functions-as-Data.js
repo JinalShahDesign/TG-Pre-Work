5a7a44ccfc0b19e8b2cb3252e53bdcf69fc525d0
Jinal Shah <>
2018-10-27 00:33:07 -0700

    no message

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Functions-as-Data.js
@@ -0,0 +1,14 @@
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+// Write your code below
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name);
+
+/*We want to assign the value of the function itself, not the value it returns when invoked. Functions are first class objects, JS functions can have properties and methods.*/
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Introduction.js
@@ -0,0 +1,4 @@
+/*In programming we can accomplish "abstraction" by
+writing functions. In addition to allowing us to reuse our code, functions help to make clear readable programs.
+Higher-order functions are functions that accept other functions as arguments and/or return functions as output.
+This allows us to build abstractions on other abstractions.*/
